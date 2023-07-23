487d69ac73446592891d73533905e2c8cb656f36
plague-boy <tommmysmith.204@gmail.com>
2023-07-24 09:42:23 +1200

    24/07/23 0.1

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,12 +1,12 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
+let rect_width  = 200;
+let rect_height = 200;
 
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.show_guide(); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
@@ -19,5 +19,67 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+  let noseX = (100)
+  let noseY = (125)
+
+fill(240, 129, 65); //Orange
+stroke(168, 74, 20); //Dusty Orange
+beginShape(); //Head
+  vertex(noseX,noseY-50);
+
+  vertex(noseX-15,noseY-50); //Left Side
+  vertex(noseX-15,noseY-85);
+  vertex(noseX-55,noseY-50);
+  vertex(noseX-65,noseY-45);
+  vertex(noseX-55,noseY-35);
+  vertex(noseX-65,noseY-25);
+  vertex(noseX-55,noseY-15);
+  vertex(noseX-70,noseY-5);
+  vertex(noseX-52,noseY+5);
+
+  vertex(noseX,noseY+20); //Bottom
+
+  vertex(noseX+52,noseY+5); //Right Side
+  vertex(noseX+70,noseY-5);
+  vertex(noseX+55,noseY-15);
+  vertex(noseX+65,noseY-25);
+  vertex(noseX+55,noseY-35);
+  vertex(noseX+65,noseY-45);
+  vertex(noseX+55,noseY-50);
+  vertex(noseX+15,noseY-85);
+  vertex(noseX+15,noseY-50);
+
+  vertex(noseX,noseY-50) //Top
+endShape(CLOSE)
+
+fill(255, 255, 255); //WHITE
+noStroke(168, 74, 20)
+beginShape(); //Little White marking
+ vertex(noseX,noseY-10); //Center Nose
+ vertex(noseX-20,noseY-20) 
+  vertex(noseX-35,noseY-15)
+  vertex(noseX-65,noseY-25);
+  vertex(noseX-55,noseY-15);
+  vertex(noseX-70,noseY-5);
+  vertex(noseX-52,noseY+5);
+  vertex(noseX,noseY+20); //Center Chin
+  vertex(noseX+52,noseY+5);
+  vertex(noseX+70,noseY-5);
+  vertex(noseX+55,noseY-15);
+  vertex(noseX+65,noseY-25);
+  vertex(noseX+35,noseY-15);
+  vertex(noseX+20,noseY-20);
+
+  vertex(noseX,noseY-10); //Center Nose
+
+endShape(CLOSE);
+
+fill(168, 74, 20);
+ellipse(noseX,noseY-12,15,5); //Nose
+
+fill(168, 74, 20); //Left Eye
+ellipse(noseX-25,noseY-35,5,15);
+
+fill(168, 74, 20); //Right Eye
+ellipse(noseX+25,noseY-35,5,15);
 }
