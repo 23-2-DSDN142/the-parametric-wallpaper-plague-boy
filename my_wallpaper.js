0a392325636ff36c76c744a99e291388ec567138
plague-boy <tommmysmith.204@gmail.com>
2023-07-28 10:16:00 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -8,9 +8,13 @@ let noseW = (15); //15 deff
 let noseH = (5); //5 deff
 let noseX = (100);//basis for location X, 100
 let noseY = (125);//basis for location Y, 125
-let foxsize = 1 //size
+let foxsize = (0.1); //size
 
-//Mushroom
+//Mushie0
+let mushsize = (1);
+let mushX = (100)
+let mushY = (100)
+
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -55,7 +59,8 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   let redC = color(168, 14, 0)
 
 //Fox Motif
-scale(foxsize); 
+push()
+scale(foxsize);
 fill(orangeA);
 stroke(crimsonA);
 beginShape(); //Head
@@ -85,14 +90,16 @@ beginShape(); //Head
 
   vertex(noseX,noseY-50) //Top
 endShape(CLOSE)
+pop()
 
-
+push()
+scale(foxsize)
 fill(whiteB); 
 stroke(crimsonA)
 beginShape(); //Little White marking
   vertex(noseX,noseY-10);
- vertex(noseX,noseY-10); //Center Nose
- vertex(noseX-20,noseY-20) 
+  vertex(noseX,noseY-10); //Center Nose
+  vertex(noseX-20,noseY-20) 
   vertex(noseX-35,noseY-15)
   vertex(noseX-65,noseY-25);
   vertex(noseX-55,noseY-15);
@@ -109,17 +116,84 @@ beginShape(); //Little White marking
   vertex(noseX,noseY-10); //Center Nose
 
 endShape(CLOSE);
+pop();
 
+push()
+scale(foxsize)
 fill(brownA);
 stroke(brownA)
 ellipse(noseX,noseY-12,noseW, noseH); //Nose
+pop()
 
+push()
+scale(foxsize)
 fill(brownA); //Left Eye
 ellipse(noseX-25,noseY-35,5,eyeWid);
+pop()
 
+push()
+scale(foxsize)
 fill(brownA); //Right Eye
 ellipse(noseX+25,noseY-35,5,eyeWid);
+pop()
 
+//Mushie
+//mush leg
+push();
+scale(mushsize);
+fill(whiteB);
+stroke(whiteC);
+beginShape();
+  vertex(mushX,mushY);
 
+  vertex(mushX+15,mushY);
+  vertex(mushX+25,mushY+25);
+  vertex(mushX+10,mushY+100);
 
+  vertex(mushX,mushY+100);
+
+  vertex(mushX-10,mushY+100);
+  vertex(mushX-25,mushY+25);
+  vertex(mushX-15,mushY);
+
+endShape(CLOSE);
+pop()
+
+push()
+scale(mushsize)
+fill(redA);
+stroke(crimsonB);
+beginShape(); //mush hat
+vertex(mushX,mushY-75); //center middle top
+
+vertex(mushX+50,mushY-75);
+vertex(mushX+100,mushY-25);
+vertex(mushX+75,mushY);
+
+vertex(mushX,mushY);
+
+vertex(mushX-75,mushY);
+vertex(mushX-100,mushY-25);
+vertex(mushX-50,mushY-75);
+
+vertex(mushX,mushY-75);
+endShape(CLOSE);
+pop()
+
+push() //Spot Uno
+scale(mushsize)
+fill(whiteA)
+stroke(crimsonB)
+beginShape()
+vertex(mushX+10,mushY-65)
+vertex (mushX+45,mushY-65)
+vertex(mushX+65,mushY-45)
+vertex(mush+75,mushY-40)
+vertex(mushX,mushY-45)
+vertex(mushX-15,mushY-55)
+vertex(mushX,mushY-65)
+vertex(mushX+10,mushY-65)
+
+endShape()
+pop()
 }
